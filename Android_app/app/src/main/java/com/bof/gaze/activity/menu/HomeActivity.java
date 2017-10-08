2af4f2b3dd2474f9f89e49d1c9c978fe341bb7d9
Grégoire Chamberland <gregoire.chamberland@gmail.com>
2017-10-08 15:09:01 +0200

    Corrected a mistake.

--- Android_app/app/src/main/java/com/bof/gaze/activity/menu/HomeActivity.java
+++ Android_app/app/src/main/java/com/bof/gaze/activity/menu/HomeActivity.java
@@ -1,10 +1,12 @@
 package com.bof.gaze.activity.menu;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -14,35 +16,31 @@ import com.bof.gaze.detection.ObjectDetector;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private ImageView splashScreenImg;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // IMAGEVIEW SPLASHSCREEN
-        splashScreenImg = (ImageView) findViewById(R.id.splashscreenImg) ;
-        splashScreenImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sharedPref = getSharedPreferences("main", Context.MODE_PRIVATE);
-                String nickname = sharedPref.getString("nickname", "");
-
-                if (nickname.isEmpty()) {
-                    Intent intent = new Intent(getApplicationContext(), NicknameActivity.class);
-                    startActivity(intent);
-                }
-                else
-                {
-                    Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                    intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
-                    startActivity(intent);
-                }
-            }
-        });
-
         ObjectDetector.getInstance().loadDetectors(this.getAssets(), "detectors");
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        SharedPreferences sharedPref = getSharedPreferences("main", Context.MODE_PRIVATE);
+        String nickname = sharedPref.getString("nickname", "");
+
+        if (nickname.isEmpty()) {
+            Intent intent = new Intent(getApplicationContext(), NicknameActivity.class);
+            startActivity(intent);
+        }
+        else
+        {
+            Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+            intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
+            startActivity(intent);
+        }
+
+        return super.onTouchEvent(event);
     }
 
     @Override
