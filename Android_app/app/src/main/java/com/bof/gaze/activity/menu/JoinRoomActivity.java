8073c098f5c60a47d401039c9890f91cc161bbb6
Grégoire Chamberland <gregoire.chamberland@gmail.com>
2018-01-22 00:33:26 +0100

    Added a way to connect an ip directly.

--- Android_app/app/src/main/java/com/bof/gaze/activity/menu/JoinRoomActivity.java
+++ Android_app/app/src/main/java/com/bof/gaze/activity/menu/JoinRoomActivity.java
@@ -1,37 +1,81 @@
 package com.bof.gaze.activity.menu;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bof.gaze.R;
-import com.bof.gaze.network.Common;
+import com.bof.gaze.activity.common.CommonGazeActivity;
 import com.bof.gaze.model.Room;
-import com.bof.gaze.network.client.RoomFinder;
+import com.bof.gaze.network.Common;
 import com.bof.gaze.network.Util;
+import com.bof.gaze.network.client.RoomFinder;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Locale;
 
-public class JoinRoomActivity extends AppCompatActivity
+public class JoinRoomActivity extends CommonGazeActivity
         implements RoomFinder.RoomListChangeListener, AdapterView.OnItemClickListener {
 
-    RoomFinder finder;
-    ListView gameList;
-    ArrayAdapter adapter;
-    ArrayList<Room> games = new ArrayList<>();
-    TextView stateBar;
+    private RoomFinder finder;
+    private ArrayAdapter adapter;
+    private ArrayList<Room> games = new ArrayList<>();
+    private TextView stateBar;
+
+    private int numberOfTouchOnStateBar;
+    private AlertDialog.Builder ipPromptDialogueBuilder;
+
+    private AlertDialog.Builder createIPPromptDialogue() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Connect IP directly");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        final JoinRoomActivity self = this;
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                try {
+                    InetAddress addr = InetAddress.getByName(input.getText().toString());
+                    Intent intent = new Intent(self, LobbyActivity.class);
+                    intent.putExtra("serverAddress", addr);
+                    intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    startActivity(intent);
+                    finish();
+                } catch (UnknownHostException e) {
+                    showToast("Cannot find the specified host");
+                } catch (Exception e) {
+                    showToast("Provided address is invalid.");
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        return builder;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +103,7 @@ public class JoinRoomActivity extends AppCompatActivity
                 Common.UDP_PORT
         );
 
-        gameList = (ListView) findViewById(R.id.gameList);
+        ListView gameList = findViewById(R.id.gameList);
         adapter = new ArrayAdapter<>(
                 this,
                 R.layout.join_game_layout_list_item,
@@ -68,11 +112,22 @@ public class JoinRoomActivity extends AppCompatActivity
         gameList.setAdapter(adapter);
         gameList.setOnItemClickListener(this);
 
-        stateBar = (TextView) findViewById(R.id.join_act_state_bar);
+        ipPromptDialogueBuilder = createIPPromptDialogue();
+
+        stateBar = findViewById(R.id.join_act_state_bar);
+        stateBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberOfTouchOnStateBar++;
+                if (numberOfTouchOnStateBar >= 10) {
+                    ipPromptDialogueBuilder.show();
+                }
+            }
+        });
 
         finder.addRoomListChangeListener(this);
 
-        ImageView backButton = (ImageView) findViewById(R.id.join_act_back_btn);
+        ImageView backButton = findViewById(R.id.join_act_back_btn);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,6 +139,8 @@ public class JoinRoomActivity extends AppCompatActivity
     @Override
     protected void onResume() {
         super.onResume();
+
+        numberOfTouchOnStateBar = 0;
 
         if (!finder.isListening()) {
             finder.startListening();
