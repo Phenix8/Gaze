4a6f2100850113e31a73533ca456e837fb4e30c3
Grégoire Chamberland <gregoire.chamberland@gmail.com>
2018-04-26 02:22:27 +0200

    Removed unused imports.

--- Android_app/app/src/main/java/com/bof/gaze/activity/menu/HomeActivity.java
+++ Android_app/app/src/main/java/com/bof/gaze/activity/menu/HomeActivity.java
@@ -13,8 +13,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bof.gaze.R;
